e482b5621faca965317b2d5df90c686ffc3b2759
Syed Faraz Khalid <36688688+Farazzaidi22@users.noreply.github.com>
2021-06-24 05:47:33 +0500

    added toggle tick functionality to tick icon to mark the peoject as complete or incomplete

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="project">
+  <div class="project" :class="{ complete: project.complete }">
       <div class="actions">
           <h3 @click="ShowDetails">{{ project.title}}</h3>
           <div class="icons">
               <span class="material-icons">edit</span>
               <span @click="deleteProject" class="material-icons">delete</span>
-              <span class="material-icons">done</span>
+              <span @click="toggleComplete" class="material-icons tick">done</span>
           </div>
       </div>
 
@@ -33,6 +33,17 @@ export default {
             fetch(this.uri, {method: 'DELETE'})
             .then(() => this.$emit('delete', this.project.id))
             .catch(err => console.log(err.message))
+        },
+        toggleComplete(){
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json'},
+                body: JSON.stringify({ complete: !this.project.complete })
+            })
+            .then(() => {
+                this.$emit('complete', this.project.id)
+            })
+            .catch((err) => console.log(err.message))
         }
     }
 }
@@ -63,5 +74,11 @@ export default {
     }
     .material-icons:hover{
         color: #777;
+    }
+    .project.complete{
+        border-left: 4px solid #00ce89;
+    }
+    .project.complete .tick{
+        color: #00ce89;
     }
 </style>
